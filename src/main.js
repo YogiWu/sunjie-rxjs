0e1fa0c184a96b83d83fa6a6da803b457b1cb71f
孙杰 <sunjie@sunjiedeMacBook-Pro.local>
2018-05-18 09:17:29 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+import { Observable } from 'rxjs/Observable'
+const fruitsObservable = Observable.create(observe => {
+  observe.next('apple')
+  observe.next('orange')
+  observe.complete()
 })
+const fruitsObserver = {
+  next: data => console.log(data),
+  complete: () => console.log('done!')
+}
+
+fruitsObservable.subscribe(fruitsObserver)
