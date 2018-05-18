e815bc7e6e0a912a042906c1be139dd7a6789b45
孙杰 <sunjie@sunjiedeMacBook-Pro.local>
2018-05-18 09:33:25 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import { Observable } from 'rxjs/Observable'
 const fruitsObservable = Observable.create(observe => {
   observe.next('apple')
   observe.next('orange')
-  observe.complete()
+  // observe.error(new Error('mistake'))
+  setTimeout(() => {
+    observe.next('lemon')
+    observe.complete()
+  }, 1000)
 })
 const fruitsObserver = {
   next: data => console.log(data),
+  error: error => console.log(error.message),
   complete: () => console.log('done!')
 }
 
-fruitsObservable.subscribe(fruitsObserver)
+console.log('----- before subscribe -----')
+const fruitsSubscription = fruitsObservable.subscribe(fruitsObserver)
+console.log('----- after subscribe -----')
+
+setTimeout(() => {
+  fruitsSubscription.unsubscribe()
+}, 500)
